9b48157c61599f5d4c91bb51963438a8b903fbeb
Juampa <juanpguirao@gmail.com>
2022-12-13 22:41:43 -0300

    Product Manager desafio nº2

--- ProductsManager.js
+++ ProductsManager.js
@@ -1,55 +1,116 @@
-class ProductManager{
-    static codeGenerator = 0;
-    constructor () {
-        this.products = [];
-    }
-    addProduct (title, description, price, thumbnail, stock, code){
-        ProductManager.codeGenerator++;
-        const newProduct={
-            id:ProductManager.codeGenerator,
-            title,
-            description,
-            price,
-            thumbnail,
-            stock,
-            code
-        }
+const fs = require('fs/promises')
+const { existsSync } = require('fs');
 
-        if(this.products.find (e => e.code === newProduct.code)){
-            console.log("Producto existente")
-            return;
-        }
+class ProductsManager {
+    constructor(path){
+        this.path = path
+    }
 
-        if (newProduct.title === (undefined)
-            ||newProduct.description === (undefined)
-            ||newProduct.price === (undefined)
-            ||newProduct.thumbnail === (undefined)
-            ||newProduct.stock === (undefined)
-            ||newProduct.code === (undefined)           ){
-            console.log( "Debe rellenar todos los campos")
-            return;}
-
-            this.products.push(newProduct);
-            return newProduct;
-    }
-    getProducts (){
-        return this.products;
-    }
-    getProductsById(idSearch){
-        const search = this.products.findIndex(e => e.id === idSearch);
-        if(search < 0){
-            console.error('Not found');
-            return;
+    async addProduct(product) {
+        try{
+            const savedProducts = await this.getProducts()
+            const duplicatedProduct = savedProducts.find(item => item.code == product.code)
+            if (duplicatedProduct){
+                throw new Error(`El producto: ${product.code} ya existe`)
+            }
+            if (Object.keys(product).length < 6) {
+                throw new Error(`Debe rellenar todos los campos`)
+            }
+            const newId = savedProducts.length > 0 ? savedProducts[savedProducts.length -1 ].id + 1 : 1
+            const newProduct = {
+                id: newId, 
+                ...product
+            }
+            savedProducts.push(newProduct)
+            const productListString = JSON.stringify(savedProducts, null, '\t')
+            await fs.writeFile(this.path, productListString)
+            console.log(`${product.title} añadido`)
         }
-        const searchProduct = this.products[search];
-        return searchProduct; 
+        catch(error){
+            console.log(error.message)
+        }
     }
-    };
-const myProductManager = new ProductManager();
+    
+    async getProducts() {
+        try{
+            if (existsSync(this.path)){
+                const products = await fs.readFile(this.path, 'utf-8')
+                if(products.length > 0){
+                    const parsedProducts = JSON.parse(products)
+                    return parsedProducts
+                }
+                else return []
+            }
+            else return []
+        }
+        catch(error){
+            console.log(error.message)
+        }
+    }
+
+    async getProductById(id) {
+        try{
+            const savedProducts = await this.getProducts();
+            const selectedProduct = savedProducts.find(prod => prod.id === id)
+            if(!selectedProduct){
+                throw new Error('No se encontro ningun producto')
+            }
+            return selectedProduct
+        }
+        catch(error){
+            console.log(error.message)
+        }
+    }
+
+    async updateProduct(id, product) {
+        try{
+            const savedProducts = await this.getProducts()
+            const targetProduct = await this.getProductById(id)
+            if(targetProduct){
+                const updatedProduct = {...targetProduct, ...product}
+                const updatedList = savedProducts.map(prod =>{
+                    if(prod.id === id){
+                        return updatedProduct
+                    }else{
+                        return prod
+                    }
+                })
+                const productListString = JSON.stringify(updatedList, null, '\t')
+                await fs.writeFile(this.path, productListString)
+                console.log('Producto modificado')
+            }
+        }
+        catch(error){
+            console.log(error.message)
+        }
+    }
+
+    async deleteProduct(id) {
+        try{
+            const savedProducts = await this.getProducts();
+            const targetProduct = await this.getProductById(id)
+            const filteredList = savedProducts.filter(prod => prod.id !== id)
+            if(!targetProduct){
+                throw new Error('no se encontro ningun producto')
+            }
+            else{
+                const productListString = JSON.stringify(filteredList, null, '\t')
+                await fs.writeFile(this.path, productListString)
+                console.log(`${targetProduct.title} eliminado`)
+            }
+        }
+        catch(error){
+            console.log(error.message)
+        }
+    }
+}
+
+const myProductManager = new ProductsManager();
 console.log(myProductManager.getProducts());
 console.log(myProductManager.addProduct( 'Prueba','Este es un producto prueba', 200, 'Sin imagen', 25,1));
 console.log(myProductManager.addProduct('Prueba', 'Este es un producto prueba', 200, 'Sin imagen', 25,2));
 console.log(myProductManager.addProduct( 'Producto prueba', 200, 'Sin imagen', 25,1));
 console.log(myProductManager.getProducts());
-console.log(myProductManager.getProductsById(1));
-console.log(myProductManager.getProductsById(1));
+console.log(myProductManager.getProductById(1));
+
+
