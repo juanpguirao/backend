bafce8d497c29fe880060d83c959e7408301c1aa
Juan Pablo <juan@gmail>
2022-12-22 17:39:18 -0300

    actualizando

--- ProductsManager.js
+++ ProductsManager.js
@@ -15,7 +15,7 @@ class ProductManager{
             code
         }
 
-        if(this.products.find (e => e.code === newProduct.code)){
+        if(this.products.find (e => e.code !== newProduct.code)){
             console.log("Producto existente")
             return;
         }
@@ -25,9 +25,8 @@ class ProductManager{
             ||newProduct.price === (undefined)
             ||newProduct.thumbnail === (undefined)
             ||newProduct.stock === (undefined)
-            ||newProduct.code === (undefined)           ){
-            console.log( "Debe rellenar todos los campos")
-            return;}
+            ||newProduct.code === (undefined))
+            {return console.log( "Debe rellenar todos los campos")}
 
             this.products.push(newProduct);
             return newProduct;
@@ -38,8 +37,7 @@ class ProductManager{
     getProductsById(idSearch){
         const search = this.products.findIndex(e => e.id === idSearch);
         if(search < 0){
-            console.error('Not found');
-            return;
+            return console.error('Not found');;
         }
         const searchProduct = this.products[search];
         return searchProduct; 
@@ -49,7 +47,7 @@ const myProductManager = new ProductManager();
 console.log(myProductManager.getProducts());
 console.log(myProductManager.addProduct( 'Prueba','Este es un producto prueba', 200, 'Sin imagen', 25,1));
 console.log(myProductManager.addProduct('Prueba', 'Este es un producto prueba', 200, 'Sin imagen', 25,2));
-console.log(myProductManager.addProduct( 'Producto prueba', 200, 'Sin imagen', 25,1));
+console.log(myProductManager.addProduct( 'Producto prueba m', 200, 'Sin imagen', 25,1));
 console.log(myProductManager.getProducts());
 console.log(myProductManager.getProductsById(1));
 console.log(myProductManager.getProductsById(1));
